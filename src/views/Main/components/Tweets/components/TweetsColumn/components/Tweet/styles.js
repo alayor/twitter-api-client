8edc386215f9aa357c92fbdabd403aa657091e63
Alonso A. Ortega <alayor3@gmail.com>
2018-11-13 13:01:26 -0700

    Add Tweet component.

---
+++ src/views/Main/components/Tweets/components/TweetsColumn/components/Tweet/styles.js
@@ -0,0 +1,18 @@
+export const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        flex: '75%',
+        background: 'black',
+        color: 'white',
+        border: 'solid white 1px',
+        padding: '1em',
+        margin: '0.5em'
+    },
+    tweet: {
+        flex: '90%',
+        overflowY: 'scroll',
+        border: 'solid red 1px',
+        padding: '.5em'
+    }
+}
--- src/views/Main/components/Tweets/components/TweetsColumn/index.js
+++ src/views/Main/components/Tweets/components/TweetsColumn/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { styles } from './styles'
 import ColumnHeader from '../_common/components/ColumnHeader'
+import Tweet from './components/Tweet'
 
 class TweetsColumn extends Component {
     render() {
@@ -8,11 +9,10 @@ class TweetsColumn extends Component {
             <div style={styles.container}>
                 <div><ColumnHeader /></div>
                 <div style={styles.tweets}>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    <Tweet/>
+                    <Tweet/>
+                    <Tweet/>
+                    <Tweet/>
                 </div>
             </div>
         );
