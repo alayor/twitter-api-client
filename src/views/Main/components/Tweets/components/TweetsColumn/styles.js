47b2efa6265d615f07e3cd956eb479cfd50292a6
Alonso A. Ortega <alayor3@gmail.com>
2018-11-13 12:55:44 -0700

    Adjust flex direction.

--- src/views/Main/components/Tweets/components/TweetsColumn/styles.js
+++ src/views/Main/components/Tweets/components/TweetsColumn/styles.js
@@ -1,13 +1,15 @@
 export const styles = {
     container: {
+        display: 'flex',
+        flexDirection: 'column',
         flex: '75%',
         background: 'black',
         color: 'white',
         border: 'solid white 1px',
         padding: '1em',
-        overflowY: 'hidden',
     },
     tweets: {
+        flex: '90%',
         overflowY: 'scroll',
         border: 'solid red 1px',
         padding: '.5em'
