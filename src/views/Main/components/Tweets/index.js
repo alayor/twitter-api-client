23567a5b976b76f741111cea93f539b043a8443e
Alonso A. Ortega <alayor3@gmail.com>
2018-11-13 13:15:02 -0700

    Create columns collection.

--- src/views/Main/components/Tweets/index.js
+++ src/views/Main/components/Tweets/index.js
@@ -1,17 +1,41 @@
-import React, { Component } from 'react';
-import { styles } from './styles'
+import React, {Component} from 'react';
+import {styles} from './styles'
 import TweetsColumn from './components/TweetsColumn'
 import EditColumn from './components/EditColumn'
 
 class Tweets extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            columnsEditing: {
+                ['1']: false,
+                ['2']: false,
+                ['3']: false,
+            },
+            columns: []
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            columns: [{
+                id: '1'
+            }, {
+                id: '2'
+            }, {
+                id: '3'
+            }]
+        })
+    }
+
     render() {
         return (
             <div style={styles.container}>
-               <EditColumn/>
-               <TweetsColumn/>
-               <TweetsColumn/>
+                {
+                    this.state.columns.map(c => this.state.columnsEditing[c.id] ? <EditColumn/> : <TweetsColumn/>)
+                }
             </div>
-        );
+        )
     }
 }
 
