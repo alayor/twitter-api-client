f7d87406ca341d92078fe92d93e829dd4538f081
Alonso A. Ortega <alayor3@gmail.com>
2018-11-13 10:28:29 -0700

    Make dive screen sized.

--- src/views/Main/components/Tweets/styles.js
+++ src/views/Main/components/Tweets/styles.js
@@ -2,6 +2,8 @@ export const styles = {
     container: {
         flex: '75%',
         display: 'flex',
-        textAlign: 'center'
+        textAlign: 'center',
+        position: 'relative',
+        bottom: '0',
     },
 }
--- src/views/Main/index.js
+++ src/views/Main/index.js
@@ -1,11 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import { styles } from './styles'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 
 class Main extends Component {
     render() {
         return (
-            <div>
+            <div style={styles.container}>
                 <Header/>
                 <Tweets/>
             </div>
