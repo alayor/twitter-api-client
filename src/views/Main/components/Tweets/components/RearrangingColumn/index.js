4e15fe5cb12f1b46e48a9276ca9a83d82dace8f8
Alonso A. Ortega <alayor3@gmail.com>
2018-11-15 15:02:42 -0700

    Improve dragging style.

--- src/views/Main/components/Tweets/components/RearrangingColumn/index.js
+++ src/views/Main/components/Tweets/components/RearrangingColumn/index.js
@@ -7,24 +7,37 @@ class RearrangingColumn extends Component {
         this.drag = this.drag.bind(this);
         this.onDrop = this.onDrop.bind(this);
     }
+
     drag = (event) => {
+        const target = event.target
         event.dataTransfer.setData("user", this.props.user)
+        setTimeout(function () {
+            target.style.visibility = "hidden"
+        }, 1)
     }
     onDrop = (event) => {
         const source = event.dataTransfer.getData("user")
         const destination = this.props.user
         this.props.swap(source, destination)
     }
+    onDragEnd = (event) => {
+        const target = event.target
+        setTimeout(function () {
+            target.style.visibility = ""
+        }, 1)
+    }
     allowDrop = (event) => {
         event.preventDefault()
     }
+
     render() {
         return (
             <div style={styles.container}
                  draggable={true}
                  onDragStart={e => this.drag(e)}
                  onDrop={e => this.onDrop(e)}
-                 onDragOver={this.allowDrop}>
+                 onDragOver={this.allowDrop}
+                 onDragEnd={e => this.onDragEnd(e)}>
                 {'<-- '}{this.props.user}{' -->'}
             </div>
         );
